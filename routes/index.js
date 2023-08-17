3b78c25b69fb3edd33ebda92ee62a5906d4c46ec
VoronVoronov <dastan@zhodas.icu>
2023-08-17 23:35:46 +0500

    fix lang in routes

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const express = require('express'),
         menusRoutes = require('./menus.routes'),
         i18n = require('i18n');
 
-router.use("/:lang", (req, res, next) => {
+router.use("/api/:lang", (req, res, next) => {
     res.cookie('lang', req.params.lang);
     i18n.setLocale(req, req.params.lang);
     next(); // Продолжить обработку маршрутов с префиксом /api/:lang
